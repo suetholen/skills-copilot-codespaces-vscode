467e61cffe9a9f213075e7d9741ca7dbf2f6a3f3
suetholen <107198772+suetholen@users.noreply.github.com>
2023-07-18 12:28:24 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,48 @@
+// Create web server
+// 1. npm init -y
+// 2. npm install express
+// 3. npm install ejs
+// 4. npm install body-parser
+// 5. npm install mysql
+// 6. npm install express-session
+// 7. npm install multer
+// 8. npm install -g nodemon
+// 9. npm install express-myconnection
+// 10. npm install express-validator
+// 11. npm install cookie-parser
+// 12. npm install express-flash
+// 13. npm install express-messages
+// 14. npm install express-handlebars
+// 15. npm install express-validator
+// 16. npm install express-session
+// 17. npm install passport
+// 18. npm install passport-local
+// 19. npm install express-myconnection
+// 20. npm install express-validator
+// 21. npm install express-session
+// 22. npm install session-file-store
+// 23. npm install multer
+// 24. npm install multer-s3
+// 25. npm install aws-sdk
+// 26. npm install bcrypt-nodejs
+// 27. npm install --save connect-flash
+// 28. npm install --save passport-local
+// 29. npm install --save passport-facebook
+// 30. npm install --save passport-twitter
+// 31. npm install --save passport-google-oauth
+// 32. npm install --save passport-linkedin-oauth2
+// 33. npm install --save passport-instagram
+// 34. npm install --save passport-spotify
+// 35. npm install --save passport-tumblr
+// 36. npm install --save passport-oauth
+// 37. npm install --save passport-openid
+// 38. npm install --save passport-oauth2
+// 39. npm install --save passport-oauth1
+// 40. npm install --save passport-windowslive
+// 41. npm install --save passport-oauth2-refresh
+// 42. npm install --save passport-oauth2-client-password
+// 43. npm install --save passport-http
+// 44. npm install --save passport-jwt
+// 45. npm install --save passport-saml
+// 46. npm install --save passport-saml-metadata
+// 47. npm install --save passport-oauth2-jwt-bearer
